e8ec8270898dbfb22b76fa3081162f30a59660ea
MikeWencel <wencel.michael@gmail.com>
2020-04-20 23:08:00 +0200

    Classes

--- Classes/attack.py
+++ Classes/attack.py
@@ -1,5 +1,20 @@
 import random
 
+
+class Enemy:
+    atkl = 60
+    atkh = 80
+
+    def getAtk(self):
+        print(self.atkl)
+
+enemy1 = Enemy()
+enemy2 = Enemy()
+enemy1.getAtk()
+enemy2.getAtk()
+
+
+'''
 playerhp = 260
 enemyattacklow = 60
 enemyattackhigh = 80
@@ -14,9 +29,11 @@ while playerhp > 0:
 
     print("Enemy strikes for ", damage, "points of damage Current HP is", playerhp)
 
-    if playerhp == 30:
-        print("You've been teleported for respawn")
-        break
+    if playerhp > 30:
+        continue
 
+    print("You've been teleported for respawn")
+    break
 
+'''
 
