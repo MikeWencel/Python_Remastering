5ef4929073569f0112f2f85f47a3f3a49f0e0f5f
MikeWencel <wencel.michael@gmail.com>
2020-04-20 20:59:12 +0200

    functions

---
+++ Instrukcje warunkowe/functions.py
@@ -0,0 +1,21 @@
+def hello():
+    print("Hello!")
+
+hello()
+
+def hello(name,proffesion):
+    print("Hello, my name is",name,"and I work as",proffesion)
+
+hello("Mike", "junior developer")
+
+
+
+def sum(first,second):
+    return first + second
+
+x = sum(8,5)
+
+print(x)
+
+
+
