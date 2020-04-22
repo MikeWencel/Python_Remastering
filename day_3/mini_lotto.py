6ac4154414887a7d6848c2ab7a7a677479b28f1e
MikeWencel <wencel.michael@gmail.com>
2020-04-22 15:38:02 +0200

    Working with new functions 'count() and randint()

--- day_3/mini_lotto.py
+++ day_3/mini_lotto.py
@@ -4,12 +4,21 @@ answer = int(input("Podaj ile liczb wylosować\n"))
 max_numbers = int(input("Podaj zakres liczb\n"))
 
 myList = []
+
 i = 0
-while i < max_numbers:
+while i < answer:
+    #Funkcja randint losuje liczbę w podanym przez nas zakresie
     number = random.randint(1,max_numbers)
-    if mylist.append(number) == 0:
+    #Funkcja count sprawdza ile razy pojawiła się ta liczba (number) w mojej liście (myList)
+    if myList.count(number) == 0:
+        #Jeśli pojawiła się 0 razy dodaj ją do listy
         myList.append(number)
+        # Zwiększam licznik i
         i = i + 1
 
-print(mylist)
+
+print("numbers: ", myList)
+
+
+
 
