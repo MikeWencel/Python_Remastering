e1250a8010723c0217f5f9d0761209cdccae07b2
MikeWencel <wencel.michael@gmail.com>
2020-05-05 13:25:37 +0200

    Another functions in Python

---
+++ List_Range_Tuple/lists.py
@@ -0,0 +1,15 @@
+# ipAddress = input("Please enter an IP addres: ")
+# print(ipAddress.count("."))
+
+parrot_list =["non pinin","no more","a stiff","bereft of live"]
+for state in parrot_list:
+    print("This parrot is " + state)
+
+even = [2,4,6,8]
+odd = [1,3,5,7,9]
+
+numbers = even + odd
+# Metoda sortująca
+numbers.sort()
+numbers.reverse()
+print(numbers)
